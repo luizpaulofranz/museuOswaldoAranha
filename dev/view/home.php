483273f639a5564cf18cb0f98b4c1630b269b69c
Pedro Sebastian Zanella Nunez <pedrosebastian90@gmail.com>
2015-07-15 17:33:22 -0300

    Estrutura inicial da página Home.

---
+++ dev/view/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Portal Museu Oswaldo Aranha</title>
+
+        <link rel="stylesheet" href="../resources/css/normalize.css">
+        <link rel="stylesheet" href="../resources/css/boot.css">
+        <link rel="stylesheet" href="../resources/css/grid.css">
+
+        <link rel="shortcut icon" href="../resources/images/favicon.png"/>
+    </head>
+    <body>
+
+        
+
+    </body>
+</html>
