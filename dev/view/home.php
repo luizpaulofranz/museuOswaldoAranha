e37ac2dbe7d8901fbdf447cf57ed96b2a378b326
Pedro Sebastian Zanella Nunez <pedrosebastian90@gmail.com>
2015-07-20 01:13:24 -0300

    Implementa frontend da seção de navegação da página Home.

--- dev/view/home.php
+++ dev/view/home.php
@@ -48,7 +48,7 @@
                 </div>
             </div>
 
-            <div class="container al-center bg-brown-white posiion_image_text_info transparence-70 font-georgia fontsize1">
+            <div class="container al-center bg-brown-white position_image_text_info transparence-70 font-georgia fontsize1">
                 <div class="content">
                     <p class="grid-16">
                         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
@@ -57,36 +57,38 @@
                 </div>
             </div>
         </section>
-
+        
         <div class="regulates_position">
-            <section>
-                <div>
-                    <article>
-                        <h2>VISITE</h2>
-                        <ul>
-                            <li><a href="" a><img src="" alt="">SAIBA COMO CHEGAR</a></li>
-                            <li><a href="" a><img src="" alt="">VEJA OS HORÁRIOS</a></li>
-                            <li><a href="" a><img src="" alt="">REGRAS DO MUSEU</a></li>
-                            <li><a href="" a><img src="" alt="">AGENDE SUA VISITA</a></li>
+            <section class="container container-padding-60 font-oswald-light font-light">
+                <div class="content">
+                    <article class="grid-4">
+                        <h2 class="header_box bg-pink-white fontsize45 al-center font-oswald-light font-thin2 header_box_shadow">VISITE</h2>
+                        <ul class="menu_section">
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/visite/map-icon.png" alt="Ícone Como Chegar"><p>SAIBA COMO </p><p>CHEGAR</p></a></li>
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/visite/horarios-icon.png" alt="Ícone Como Horários"><p>VEJA OS</p><p> HORÁRIOS</p></a></li>
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/visite/regras-icon.png" alt="Ícone Regras"><p>REGRAS </p><p>DO MUSEU</p></a></li>
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/visite/agende-icon.png" alt="Ícone Agende Sua Visita"><p>AGENDE SUA</p><p> VISITA</p></a></li>
                         </ul>
                     </article>
-                    <article>
-                        <h2>APRENDA</h2>
-                        <ul>
-                            <li><a href="" a><img src="" alt="">HISTÓRIA DE OSWALDO A.</a></li>
-                            <li><a href="" a><img src="" alt="">ACERVO DO MUSEU</a></li>
-                            <li><a href="" a><img src="" alt="">ACERVO CIENTÍFICO</a></li>
-                            <li><a href="" a><img src="" alt="">VEJA NOSSOS ARTIGOS</a></li>
+                    <article class="grid-4">
+                        <h2 class="header_box bg-pink-white fontsize45 al-center font-oswald-light font-thin2 header_box_shadow">APRENDA</h2>
+                        <ul class="menu_section">
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/aprenda/history-icon.png" alt="Ícone História de Oswaldo Aranha"><p>HISTÓRIA DE </p><p>OSWALDO A.</p></a></li>
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/aprenda/acervo-icon.png" alt="Ícone Acervo do Museu"><p>ACERVO DO</p><p> MUSEU</p></a></li>
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/aprenda/article-icon.png" alt="Ícone Acervo Científico"><p>ACERVO </p><p>CIENTÍFICO</p></a></li>
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/aprenda/blog-post-icon.png" alt="Ícone Postagens"><p>VEJA NOSSOS</p><p> ARTIGOS</p></a></li>
                         </ul>
                     </article>
-                    <article>
-                        <h2>EVENTOS</h2>
-                        <li><a href="" a><img src="" alt="">PRÓXIMOS EVENTOS</a></li>
+                    <article class="grid-4">
+                        <h2 class="header_box bg-pink-white fontsize45 al-center font-oswald-light font-thin2 header_box_shadow">EVENTOS</h2>
+                        <ul class="menu_section">
+                            <li><a href="" a><img src="" alt=""><img src="../resources/images/home/eventos/events-icon.png" alt="Ícone Eventos"><p>PRÓXIMOS </p><p>EVENTOS</p></a></li>
+                        </ul>
                     </article>
-                    <article>
-                        <h2>JOGO DO MUSEU</h2>
-                        <p>Confira o jogo feito em homenagem ao museu Oswaldo Aranha.</p>
-                        <a href="">JOGUE AGORA</a>
+                    <article class="grid-4 game_box">
+                        <h2 class="al-center fontsize60"><p>JOGO DO MUSEU</p></h2>
+                        <p class="al-center fontsize1b" style="margin-top: 30px;">Confira o jogo feito em homenagem ao museu Oswaldo Aranha.</p>
+                        <a href="" class="btn_game bg-pink-white header_box_shadow ds-block al-center fontsize30 font-bold">JOGUE AGORA</a>
                     </article>
                 </div>
             </section>
