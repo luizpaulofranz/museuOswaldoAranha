1a2c98d4fc37cf85640917621e8f19dba4ed80a0
Pedro Sebastian Zanella Nunez <pedrosebastian90@gmail.com>
2015-07-19 22:00:10 -0300

    Implementa topo da página Home.

--- dev/view/home.php
+++ dev/view/home.php
@@ -12,138 +12,150 @@
     </head>
     <body>
 
-        <header>
-            <div>
-                <div>
-                    <ul>
-                        <ul>
-                            <li><a src="" alt=""><img src="" alt="">Praça Getúlio Vargas nº 585 - centro - Alegrete RS</a></li>
-                            <li><a src="" alt=""><img src="" alt="">(55) 3422 xxxx</a></li>
-                            <li><a src="" alt=""><img src="" alt=""></a></li>
-                        </ul>
-                    </ul>
-                </div>
-                <div>
-                    <img src="" alt="">                        
-                    <ul class="font font-menu">
-                        <li><a src="" alt="">INÍCIO</a></li>
-                        <li><a src="" alt="">SOBRE</a></li>
-                        <li><a src="" alt="">EVENTOS</a></li>
-                        <li><a src="" alt="">CONTATO</a></li>
-                        <li><a src="" alt="">NOTÍCIAS</a></li>
-                        <li><a src="" alt="">APRENDA</a></li>
-                        <li><a src="" alt="">AMIGOS DO MUSEU</a></li>
-                        <li><a src="" alt="">VISITE</a></li>
-                    </ul>
-                </div>
+        <div class="container bg-white-grey menu-height2">
+            <div class="content">
+                <ul class="font-oswald-light font-bold fontsize090 fl-right text-uppercase">
+                    <li class="list-info"><a src="" alt=""><img src="../resources/images/home/endereco-icon.png" class="fl-left img-micro" alt="Icone Encereço">Praça Getúlio Vargas nº 585 - centro - Alegrete RS</a></li>
+                    <li class="list-info"><a src="" alt=""><img src="../resources/images/home/telefone-icon.png" class="fl-left img-micro" alt="Icone Telefone">(55) 3422 xxxx</a></li>
+                    <li class="list-info"><a src="" alt=""><img src="../resources/images/home/facebook-icon.png" alt="Nossa Página no Facebook"></a></li>
+                </ul>
+            </div>
+        </div>
 
-                <div>
-                    <div>
-                        <p>
-                        
-                        </p>
-                    </div>
-                </div>
+        <header class="container bg-black menu-height section-header transparence-85">
+            <div class="content">
+                <h1 class="fontzero">
+                    Portal Museu Oswaldo Aranha
+                    <img src="../resources/images/home/logo-museu.png" alt="Museu Oswaldo Aranha" class="ds-block fl-left grid-2">                        
+                </h1>
+                <ul class="font-oswald-light font-light fontsize1b fl-right">
+                    <li class="menu-item"><a src="" alt="">INÍCIO</a></li>
+                    <li class="menu-item"><a src="" alt="">SOBRE</a></li>
+                    <li class="menu-item"><a src="" alt="">EVENTOS</a></li>
+                    <li class="menu-item"><a src="" alt="">CONTATO</a></li>
+                    <li class="menu-item"><a src="" alt="">NOTÍCIAS</a></li>
+                    <li class="menu-item"><a src="" alt="">APRENDA</a></li>
+                    <li class="menu-item"><a src="" alt="">AMIGOS DO MUSEU</a></li>
+                    <li class="menu-item"><a src="" alt="">VISITE</a></li>
+                </ul>
             </div>
         </header>
 
         <section>
-            <div>
-                <article>
-                    <h2>VISITE</h2>
-                    <ul>
-                        <li><a href="" a><img src="" alt="">SAIBA COMO CHEGAR</a></li>
-                        <li><a href="" a><img src="" alt="">VEJA OS HORÁRIOS</a></li>
-                        <li><a href="" a><img src="" alt="">REGRAS DO MUSEU</a></li>
-                        <li><a href="" a><img src="" alt="">AGENDE SUA VISITA</a></li>
-                    </ul>
-                </article>
-                <article>
-                    <h2>APRENDA</h2>
-                    <ul>
-                        <li><a href="" a><img src="" alt="">HISTÓRIA DE OSWALDO A.</a></li>
-                        <li><a href="" a><img src="" alt="">ACERVO DO MUSEU</a></li>
-                        <li><a href="" a><img src="" alt="">ACERVO CIENTÍFICO</a></li>
-                        <li><a href="" a><img src="" alt="">VEJA NOSSOS ARTIGOS</a></li>
-                    </ul>
-                </article>
-                <article>
-                    <h2>EVENTOS</h2>
-                    <li><a href="" a><img src="" alt="">PRÓXIMOS EVENTOS</a></li>
-                </article>
-                <article>
-                    <h2>JOGO DO MUSEU</h2>
-                    <p>Confira o jogo feito em homenagem ao museu Oswaldo Aranha.</p>
-                    <a href="">JOGUE AGORA</a>
-                </article>
+            <div class="container section-1">
+                <div class="content">
+
+                </div>
+            </div>
+
+            <div class="container al-center bg-brown-white posiion_image_text_info transparence-70 font-georgia fontsize1">
+                <div class="content">
+                    <p class="grid-16">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                        The point of using Lorem Ipsum  is that it has a more-or-less normal distribution of letters, as opposed to using.
+                    </p>
+                </div>
             </div>
         </section>
 
-        <section>
-            <blockquote class="quote-externo">
-                <p>"A tradição é a experiência dos povos consagrada pelo tempo."</p>
-                <cite>Oswaldo Aranha</cite>
-            </blockquote>
-        </section>
-        
-        <section>
-            <header>
-                <h2>NOVIDADES</h2>
-            </header>
-            
-            <article>
-                <img src="" alt="">
-                <p></p>
-            </article>
-            <article>
-                <img src="" alt="">
-                <p></p>
-            </article>
-            <article>
-                <img src="" alt="">
-                <p></p>
-            </article>
-            <article>
-                <img src="" alt="">
-                <p></p>
-            </article>
-            <article>
-                <img src="" alt="">
-                <p></p>
-            </article>
-            <article>
-                <img src="" alt="">
-                <p></p>
-            </article>
-            
-        </section>
-        
-        <section>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </section>
-        
-        <footer>
-            <div>
-                <ul>
-                    <li><a href="">INÍCIO</a></li>
-                    <li><a href="">SOBRE</a></li>
-                    <li><a href="">EVENTOS</a></li>
-                    <li><a href="">CONTATO</a></li>
-                    <li><a href="">NOTÍCIAS</a></li>
-                    <li><a href="">APRENDA</a></li>
-                    <li><a href="">AMIGOS DO MUSEU</a></li>
-                    <li><a href="">VISITE</a></li>
-                    <li><a href="">MAPA DO SITE</a></li>
-                </ul>
-            </div>
-            
-            <div>
-                <p>MOA - Museu Oswaldo Aranha - 2015</p>
-            </div>
-        </footer>
+        <div class="regulates_position">
+            <section>
+                <div>
+                    <article>
+                        <h2>VISITE</h2>
+                        <ul>
+                            <li><a href="" a><img src="" alt="">SAIBA COMO CHEGAR</a></li>
+                            <li><a href="" a><img src="" alt="">VEJA OS HORÁRIOS</a></li>
+                            <li><a href="" a><img src="" alt="">REGRAS DO MUSEU</a></li>
+                            <li><a href="" a><img src="" alt="">AGENDE SUA VISITA</a></li>
+                        </ul>
+                    </article>
+                    <article>
+                        <h2>APRENDA</h2>
+                        <ul>
+                            <li><a href="" a><img src="" alt="">HISTÓRIA DE OSWALDO A.</a></li>
+                            <li><a href="" a><img src="" alt="">ACERVO DO MUSEU</a></li>
+                            <li><a href="" a><img src="" alt="">ACERVO CIENTÍFICO</a></li>
+                            <li><a href="" a><img src="" alt="">VEJA NOSSOS ARTIGOS</a></li>
+                        </ul>
+                    </article>
+                    <article>
+                        <h2>EVENTOS</h2>
+                        <li><a href="" a><img src="" alt="">PRÓXIMOS EVENTOS</a></li>
+                    </article>
+                    <article>
+                        <h2>JOGO DO MUSEU</h2>
+                        <p>Confira o jogo feito em homenagem ao museu Oswaldo Aranha.</p>
+                        <a href="">JOGUE AGORA</a>
+                    </article>
+                </div>
+            </section>
 
+            <section>
+                <blockquote class="quote-externo">
+                    <p>"A tradição é a experiência dos povos consagrada pelo tempo."</p>
+                    <cite>Oswaldo Aranha</cite>
+                </blockquote>
+            </section>
+
+            <section>
+                <header>
+                    <h2>NOVIDADES</h2>
+                </header>
+
+                <article>
+                    <img src="" alt="">
+                    <p></p>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <p></p>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <p></p>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <p></p>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <p></p>
+                </article>
+                <article>
+                    <img src="" alt="">
+                    <p></p>
+                </article>
+
+            </section>
+
+            <section>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </section>
+
+            <footer>
+                <div>
+                    <ul>
+                        <li><a href="">INÍCIO</a></li>
+                        <li><a href="">SOBRE</a></li>
+                        <li><a href="">EVENTOS</a></li>
+                        <li><a href="">CONTATO</a></li>
+                        <li><a href="">NOTÍCIAS</a></li>
+                        <li><a href="">APRENDA</a></li>
+                        <li><a href="">AMIGOS DO MUSEU</a></li>
+                        <li><a href="">VISITE</a></li>
+                        <li><a href="">MAPA DO SITE</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p>MOA - Museu Oswaldo Aranha - 2015</p>
+                </div>
+            </footer>
+        </div>
     </body>
 </html>
