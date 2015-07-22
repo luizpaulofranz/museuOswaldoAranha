ea718502b968c55f9563a05e93a5c6f2107e7d4c
paulamaisa <paulinhamaisa@hotmail.com>
2015-07-22 20:55:22 -0300

    Começando a pagina sobre e contato

---
+++ dev/view/sobre.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sobre</title>
+
+        <link rel="stylesheet" href="../resources/css/normalize.css">
+        <link rel="stylesheet" href="../resources/css/boot.css">
+        <link rel="stylesheet" href="../resources/css/grid.css">
+
+        <link rel="shortcut icon" href="../resources/images/favicon.png"/>
+    </head>
+    <body>
+
+        <div class="container bg-white-grey menu-height2">
+            <div class="content">
+                <ul class="font-oswald-light font-bold fontsize090 fl-right text-uppercase">
+                    <li class="list-info"><a src="" alt=""><img src="../resources/images/home/endereco-icon.png" class="fl-left img-micro" alt="Icone Encereço">Praça Getúlio Vargas nº 585 - centro - Alegrete RS</a></li>
+                    <li class="list-info"><a src="" alt=""><img src="../resources/images/home/telefone-icon.png" class="fl-left img-micro" alt="Icone Telefone">(55) 3422 xxxx</a></li>
+                    <li class="list-info"><a src="" alt=""><img src="../resources/images/home/facebook-icon.png" alt="Nossa Página no Facebook"></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <header class="container bg-black menu-height section-header transparence-85">
+            <div class="content">
+                <h1 class="fontzero">
+                    Portal Museu Oswaldo Aranha
+                    <img src="../resources/images/home/logo-museu.png" alt="Museu Oswaldo Aranha" class="ds-block fl-left grid-2">                        
+                </h1>
+                <ul class="font-oswald-light font-light fontsize1b fl-right">
+                    <li class="menu-item"><a src="" alt="">INÍCIO</a></li>
+                    <li class="menu-item"><a src="" alt="">SOBRE</a></li>
+                    <li class="menu-item"><a src="" alt="">EVENTOS</a></li>
+                    <li class="menu-item"><a src="" alt="">CONTATO</a></li>
+                    <li class="menu-item"><a src="" alt="">NOTÍCIAS</a></li>
+                    <li class="menu-item"><a src="" alt="">APRENDA</a></li>
+                    <li class="menu-item"><a src="" alt="">AMIGOS DO MUSEU</a></li>
+                    <li class="menu-item"><a src="" alt="">VISITE</a></li>
+                </ul>
+            </div>
+        </header>
+        <section>
+            <div class="container section-Sobre-1">
+                <div class="content">
+
+                </div>
+            </div>
+
+            <div class="container al-center bg-brown-white position_image_text_info transparence-70 font-georgia fontsize1">
+                <div class="content">
+                    <p class="grid-16">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                        The point of using Lorem Ipsum  is that it has a more-or-less normal distribution of letters, as opposed to using.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <div class="regulates_position">
+
+
+            <section class="container container-padding-60 font-oswald-light font-light">
+                <div class="content">
+                    <div>
+                        <h2 class="font-oswald-light fontsize30 " style="text-align: center; padding-bottom: 3em" >SOBRE</h2>
+                        <img src="../resources/images/sobre/oswaldoAranhaSobre.jpg" alt="lalala" style="width: 20em; height: 25em">
+                        
+                        <blockquote class="font-georgia">
+                    
+                        <p class="fontsize1b" style="a">
+                            Na primeira sessão especial da Assembléia Geral da ONU, em 1947, Oswaldo Aranha inaugurou a tradição -que se mantém até hoje- de ser um brasileiro o primeiro orador daquele foro internacional.
+
+                            Oswaldo Euclides de Souza Aranha era um dos 11 filhos do coronel Euclides de Souza Aranha e de Luiza de Freitas Valle Aranha, proprietários da estância Alto Uruguai, no município gaúcho de Itaqui.
+
+                            Freqüentou a faculdade de direito, aproximando-se dos colegas que se ligavam às oposições, embora o pai fosse do partido situacionista. Manteve também intensa atividade política contra o então presidente da República, marechal Hermes da Fonseca.
+
+                            Em princípios de 1917, instalou banca em Uruguaiana. Até 1923, dedicou-se quase exclusivamente à advocacia, obtendo alto conceito profissional. Getúlio Vargas (também advogado, formado em 1907) costumava consultá-lo, e os dois chegaram a ter clientes em comum.
+
+                            Pouco depois de haver fixado residência em Uruguaiana, Aranha casou com Delminda Benvinda Gudolle, com quem teria quatro filhos.
+                        </p>
+                        </blockquote>
+                    </div>
+
+                </div>
+            </section>
+
+            <section class="container container-padding-60 bg-pink-white">
+
+                <div class="content al-center">
+                    <header style="background: #ffffff; height: 5px">
+                        <h2 class="fontsize25 font-oswald-light al-center ds-inblock shadow_parceiros">PARCEIROS</h2>;
+                    </header>
+
+                    <div style="margin-top: 40px;">
+                        <div class="grid-8 al-center" style="height: 180px;">
+                            <div class="border-red" style="height: 180px; background: #ffffff;">
+                                <img class="ds-inline fl-none" style="margin-top: 20px;" src="../resources/images/home/unipampa.png" alt="Logotipo Unipampa">
+                            </div>
+                        </div>
+                        <div class="grid-8 al-center" style="height: 180px;">
+                            <div class="border-red" style="height: 180px; background: #ffffff;">
+                                <img class="ds-inline fl-none" style="margin-top: 40px;" src="../resources/images/home/prefeitura.png" alt="Logotipo Unipampa">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <footer class="container container-padding-60 bg-black">
+                <div class="content">
+                    <ul class="font-oswald-light font-light fontsize1b grid-16 al-center">
+                        <li class="menu-item2 fontsize18"><a src="" alt="">INÍCIO</a></li>
+                        <li class="menu-separador fontsize1">/</li>
+                        <li class="menu-item2 fontsize18"><a src="" alt="">SOBRE</a></li>
+                        <li class="menu-separador fontsize1">/</li>
+                        <li class="menu-item2 fontsize18"><a src="" alt="">EVENTOS</a></li>
+                        <li class="menu-separador fontsize1">/</li>
+                        <li class="menu-item2 fontsize18"><a src="" alt="">CONTATO</a></li>
+                        <li class="menu-separador fontsize1">/</li>
+                        <li class="menu-item2 fontsize18"><a src="" alt="">NOTÍCIAS</a></li>
+                        <li class="menu-separador fontsize1">/</li>
+                        <li class="menu-item2 fontsize18"><a src="" alt="">APRENDA</a></li>
+                        <li class="menu-separador fontsize1">/</li>
+                        <li class="menu-item2 fontsize18"><a src="" alt="">AMIGOS DO MUSEU</a></li>
+                        <li class="menu-separador fontsize1">/</li>
+                        <li class="menu-item2 fontsize18"><a src="" alt="">VISITE</a></li>
+                        <li class="menu-separador fontsize1">/</li>
+                        <li class="menu-item2 fontsize18"><a src="" alt="">MAPA DO SITE</a></li>
+                    </ul>
+                </div>
+            </footer>
+
+            <div class="container" style="padding-top: 15px; padding-bottom: 15px;">
+                <div class="content al-center">
+                    <p class="font-georgia fontsize1">MOA - Museu Oswaldo Aranha - 2015</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
