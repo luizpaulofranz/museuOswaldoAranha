e199168a384c1310a06b25763323a682c8109560
anne <lizeescarrone@hotmail.com>
2015-08-05 00:10:24 -0300

    ajustando commit

--- dev/view/contato.php
+++ dev/view/contato.php
@@ -14,14 +14,13 @@
 
         <div class="container bg-white-grey menu-height2">
             <div class="content">
-                 <ul class="font-oswald-light font-bold fontsize090 fl-right text-uppercase">
+                <ul class="font-oswald-light font-bold fontsize090 fl-right text-uppercase">
                     <li class="list-info"><a src="" alt=""><img src="../resources/images/home/endereco-icon.png" class="fl-left img-micro" alt="Icone Encereço" title="Endereço do Museu">Praça Getúlio Vargas nº 585 - centro - Alegrete RS</a></li>
                     <li class="list-info"><a src="" alt=""><img src="../resources/images/home/telefone-icon.png" class="fl-left img-micro" alt="Icone Telefone" title="Telefone para Contato">(55) 3422 xxxx</a></li>
                     <li class="list-info"><a src="" alt=""><img src="../resources/images/home/facebook-icon.png" alt="Nossa Página no Facebook" title="Facebook"></a></li>
                 </ul>
             </div>
         </div>
-
         <header class="container bg-black menu-height section-header transparence-85">
             <div class="content">
                 <h1 class="fontzero">
@@ -46,7 +45,6 @@
 
                 </div>
             </div>
-
             <div class="container al-center bg-brown-white position_image_text_info transparence-70 font-georgia fontsize1">
                 <div class="content">
                     <p class="grid-16">
@@ -56,51 +54,93 @@
                 </div>
             </div>
         </section>      
-
-
         <div class="regulates_position">
-
-
             <section class="container container-padding-60 font-oswald-light font-light">
                 <div class="content">
                     <div>
                         <h2 class="font-oswald-light fontsize30 " style="text-align: center; padding-bottom: 3em" >ENTRE EM CONTATO</h2>
-
-
-
-
-
-
-                        <body> <form name="meu_form">
-                                
-                                <p class="nome">
-                                    <input type="text" id="nomeid" placeholder="Tiago Vale" required="required" name="nome" />
-                                    <label for="nome">Nome</label> 
-                                </p> <p class="fone"> 
-                                    <input type="text" id="foneid" placeholder="(xx)xx-xx-xx-xx" name="fone" /> 
-                                    <label for="fone">Fone</label> 
-                                </p> 
-                                <p> <input type="email" id="emailid" placeholder="fulano@mail.com" name="email" /> 
-                                    <label for="email">Email</label>
-                                </p> 
-                                <p> <textarea placeholder="Deixe sua opnião">
+                        <body> 
+                            <section class=" al-left font-oswald-light news_description fontsize18 ds-block border-grey">                                
+                                <form name="meu_form">
+                                        <p class="nome">
+                                            <label for="nome">Nome: </label> 
+                                            <input type="text" id="nomeid" placeholder="Tiago Vale" required="required" name="nome" />
+                                        </p>
+                                        <p class="idade"> <label for="idade">Idade: </label>
+                                            <input type="text" id="idade" placeholder="18" name="idade" /> 
+                                        </p> 
+                                        <p class="escolaridade"> 
+                                            <label for="escolaridade">Escolaridade: </label>
+                                            <input type="text" id="escolaridade" placeholder="Ensio Médio" name="escolaridade" /> 
+                                        </p>
+                                        <p> <label for="profissao">Profissão: </label>
+                                            <input type="text" id="profissao" placeholder="Professor" name="Profissao" /> 
+                                        </p>
+                                        <p> <label for="genero">Gênero: </label>
+                                            <input type="radio" id="genero"  name="sexo" value="masculino" /> Masculino
+                                            <input type="radio" id="genero"  name="sexo" value="feminino" /> Feminino
+                                        </p>
+                                        <p> <label for="raca">Raça: </label>
+                                            <select name="raca">
+                                                <option value="branca">Branca</option>
+                                                <option value="negro">Negro</option>
+                                                <option value="amarela">Amarela</option>
+                                                <option value="pardo">Pardo</option>
+                                            </select>
+                                        </p>
+                                        <p class="estado"> 
+                                            <label for="estado">Estado: </label>
+                                            <select name="estado">
+                                                <option value="AC">AC</option>
+                                                <option value="AL">AL</option>
+                                                <option value="AP">AP</option>
+                                                <option value="AM">AM</option>
+                                                <option value="BA">BA</option>
+                                                <option value="CE">CE</option>
+                                                <option value="DF">DF</option>
+                                                <option value="ES">ES</option>
+                                                <option value="GO">GO</option>
+                                                <option value="NA">NA</option>
+                                                <option value="MT">MT</option>
+                                                <option value="MS">MS</option>
+                                                <option value="MG">MG</option>
+                                                <option value="PA">PA</option>
+                                                <option value="PB">PB</option>
+                                                <option value="PR">PR</option>
+                                                <option value="PE">PE</option>
+                                                <option value="PI">PI</option>
+                                                <option value="RJ">RJ</option>
+                                                <option value="RM">RM</option>
+                                                <option value="RS">RS</option>
+                                                <option value="RO">RO</option>
+                                                <option value="RR">RR</option>
+                                                <option value="SC">SC</option>
+                                                <option value="SP">SP</option>
+                                                <option value="SE">SE</option>
+                                                <option value="TO">TO</option>
+                                            </select>
+                                        </p> 
+                                        <p class="cidade"> 
+                                            <label for="cidade">Cidade: </label> 
+                                            <input type="text" id="cidade" placeholder="Alegrete" name="alegrete" /> 
+                                        </p> 
+                                        <p class="fone"> 
+                                            <label for="fone">Telefone: </label> 
+                                            <input type="text" id="foneid" placeholder="(xx)xx-xx-xx-xx" name="fone" /> 
+                                        </p> 
+                                        <p class="email"> <label for="email">Email: </label>
+                                            <input type="text" id="emailid" placeholder="fulano@mail.com" name="email" /> 
+                                        </p>
+                                        <p> <label for="opiniao">Deixei sua opinião: </label>
+                                            <textarea placeholder="Deixe sua opnião">
                                     
-                                    </textarea> </p> <p class="submit"> <input type="submit" onclick="Enviar();" value="Enviar" /> 
-                                </p> 
-                            </form>
+                                            </textarea> </p>
+                                            <p class="submit"> <input type="submit" onclick="Enviar();" value="Enviar" /> 
+                                        </p> 
+                                    </form>
+                            </section>
                         </body>
-
-
-
-
-
-
-
-
-
                     </div>
-
-
                 </div>
             </section>
 
