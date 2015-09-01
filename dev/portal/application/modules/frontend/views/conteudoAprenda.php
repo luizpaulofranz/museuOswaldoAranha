a32a8067cb3367662f84f3cc047d7796e27b49d1
paulamaisa <paulamaisap@gmail.com>
2015-09-01 02:00:23 -0300

    adiconando modificações de conteudo no aprenda e tbm modificação do link do acervo na home.

--- dev/portal/application/modules/frontend/views/conteudoAprenda.php
+++ dev/portal/application/modules/frontend/views/conteudoAprenda.php
@@ -1,18 +1,18 @@
 <section class="container container-padding-60 font-oswald-light font-light">
     <div class="content">
         <article class="grid-1-3 game_box">
-            <h2 class="al-center fontsize25"><p>ACERVO DO MUSEU</p></h2>
-            <p class="al-center fontsize1b" style="margin-top: 10px;">Descrição <br> Descrição <br>Descrição<br>Descrição<br>Descrição<br>Descrição</p>
+            <h2 class="al-center fontsize25"><p>ACERVO DO MUSEU</p></h2><br>
+            <p class="al-center fontsize1b" style="margin-top: 10px;">Confira objetos, documentos <br>descrição <br> descrição.</p><br>
             <a href="<?php echo site_url('conteudo/acervo-museu')?>" class="btn_game2 bg-pink-white header_box_shadow ds-block al-center fontsize30 font-bold">ACESSE</a>
         </article>
         <article class="grid-1-3 game_box">
-            <h2 class="al-center fontsize25"><p>ACERVO CIENTÍFICO</p></h2>
-            <p class="al-center fontsize1b" style="margin-top: 10px;">Descrição<br> Descrição <br>Descrição<br>Vice-Diretor Secretário<br>Diretor Tesoureiro<br>Vice-Diretor Financeiro</p>
+            <h2 class="al-center fontsize25"><p>ACERVO CIENTÍFICO</p></h2><br>
+            <p class="al-center fontsize1b" style="margin-top: 10px;">Confira os artigos  <br>descrição <br> descrição.</p><br>
             <a href="<?php echo site_url('acervo-cientifico')?>" class="btn_game2 bg-pink-white header_box_shadow ds-block al-center fontsize30 font-bold">ACESSE</a>  
         </article>
         <article class="grid-1-3 game_box">
-            <h2 class="al-center fontsize25"><p>JOGO DO MUSEU</p></h2>
-            <p class="al-center fontsize1b" style="margin-top: 10px;">Presidente <br> Vice-Presidente <br>Diretor-Secretário<br>Vice-Diretor Secretário<br>Diretor Tesoureiro<br>Vice-Diretor Financeiro</p>
+            <h2 class="al-center fontsize25"><p>JOGO DO MUSEU</p></h2><br>
+            <p class="al-center fontsize1b" style="margin-top: 10px;">Confira o jogo  <br>feito em homenagem ao <br> Oswaldo Aranha.</p> <br>
             <a href="aprenda_jogo_museu.php" class="btn_game2 bg-pink-white header_box_shadow ds-block al-center fontsize30 font-bold">JOGUE AGORA</a>
         </article>
 
