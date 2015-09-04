ab5179263afc95844f39ff131e76668b1bc53707
paulamaisa <paulinhamaisa@hotmail.com>
2015-09-04 03:11:19 -0300

    adicionando conteúdo e pdf do estatuto na pagina amigos do museu.

--- dev/portal/application/modules/frontend/views/conteudoAmigos.php
+++ dev/portal/application/modules/frontend/views/conteudoAmigos.php
@@ -1,17 +1,22 @@
 <section class="container container-padding-60 font-oswald-light font-light">
     <div class="content">
+        <header class="content" style="background: #993333; height: 5px; text-align: center;">
+            <h2 class="fontsize30 font-vinho font-oswald-light al-center ds-inblock news_style">ASSOCIAÇÃO DOS AMIGOS DO MUSEU</h2>;
+        </header>
+        <br><br><br>
 
         <article class="grid-1-3 game_box">
             <h2 class="al-center fontsize25"><p>DIRETORIA:</p></h2>
-            <p class="al-center fontsize1b" style="margin-top: 10px;">Presidente - Anderson Corrêa<br> Vice-Presidente - Anderson Corrêa <br>Diretor-Secretário - Anderson Corrêa<br>Vice-Diretor Secretário - Anderson <br>Diretor Tesoureiro - Anderson Corrêa<br>Vice-Diretor Financeiro - Anderson </p>
+            <p class="al-center fontsize1b" style="margin-top: 10px;">Presidente <br>Thiago Araújo Vaucher<br>Vice-Presidente<br>Jorge Augusto Peres Moojen<br>Primeiro Secretário<br>Clóvis da Costa Trindade<br>Segundo Secretário<br>Luiz Demolvão Irala Abreu</p>
+        </article>
+        <article class="grid-1-3 game_box">
+            <p class="al-center fontsize1b" style="margin-top: 10px;"><br>Primeiro Tesoureiro<br>José Rubens Rosa Pillar<br>Advogado OAB/RS 60.705<br>Segundo Tesoureiro<br>Batista Gardineli Alvarenga<br><br>Conselho Fiscal<br>Edegar de Oliveira Lopes<br>Diego Garcia Braga<br>Andressa Anhaia Benites</p>
         </article>
         <article class="grid-1-3 game_box">
             <h2 class="al-center fontsize25"><p>ESTATUTO:</p></h2>
-            <p class="al-center fontsize1b" style="margin-top: 10px;">link pdf ou ...</p>
-        </article>
-        <article class="grid-1-3 game_box">
+            <a target="_blank" href="assets/images/amigosMuseu/Estatuto.pdf" title="Leia o estatuto da associação"><p class="al-center fontsize1b"style="margin-top: 10px;">Clique aqui e leia o estatuto.</p></a>
             <h2 class="al-center fontsize25"><p>CONTATO:</p></h2>
-            <p class="al-center fontsize1b" style="margin-top: 10px;">Email: <br> Telefone:</p>
+            <p class="al-center fontsize1b" style="margin-top: 10px;">Thiago Araújo Vaucher<br>(Presidente)<br>Telefone: (55)91 56 16 52<br>E-mail: vaucher.jur@hotmail.com</p>
         </article> 
 
         <!--<article class="grid-1-3">
