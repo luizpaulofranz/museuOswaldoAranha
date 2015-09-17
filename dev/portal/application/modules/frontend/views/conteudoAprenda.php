530566fb6f9d74e5fc08d724ef8fa23dbbe437da
paulamaisa <paulinhamaisa@hotmail.com>
2015-09-17 19:23:10 -0300

    alterações de fonte, imagens e css

--- dev/portal/application/modules/frontend/views/conteudoAprenda.php
+++ dev/portal/application/modules/frontend/views/conteudoAprenda.php
@@ -1,7 +1,7 @@
 <section class="container container-padding-60 font-oswald-light font-light">
     <div class="content">
         <article class="grid-1-3 game_box">
-            <h2 class="al-center fontsize25"><p>ACERVO DO MUSEU</p></h2>
+            <h2 class="al-center fontsize25"tabindex=""><p>ACERVO DO MUSEU</p></h2>
             <p class="al-center fontsize1b" style="margin-top: 10px;">Confira obejtos... <br> documentos... <br>Descrição<br>Descrição<br>Descrição<br>Descrição</p>
             <a href="<?php echo site_url('conteudo/acervo-museu')?>" class="btn_game2 bg-pink-white header_box_shadow ds-block al-center fontsize30 font-bold">ACESSE</a>
         </article>
