f2061a7a23777172c39892a3fdf7eb78d61781de
paulamaisa <paulinhamaisa@hotmail.com>
2015-09-17 19:50:26 -0300

    ajustando tamanho das imagens do cabeçalho

--- dev/portal/application/modules/frontend/views/capaInicial.php
+++ dev/portal/application/modules/frontend/views/capaInicial.php
@@ -2,9 +2,9 @@
     <div class="container" style="margin-top: -89px; z-index: 0;">
         <figure id="slide"> 
             <img src="<?php echo site_url('assets/images/home/carrossel/imagemCarrossel1.jpg') ?>" alt="Imagem das Ilhas"style="height:569px"> 
-            <img src="<?php echo site_url('assets/images/home/carrossel/imagemCarrossel2.jpg') ?>" alt="Teste de Imagem"style="height:676px"> 
-            <img src="<?php echo site_url('assets/images/home/carrossel/imagemCarrossel3.jpg') ?>" alt="Teste de Imagem"style="height:592px"> 
-            <img src="<?php echo site_url('assets/images/home/carrossel/imagemCarrossel4.jpg') ?>" alt="Teste de Imagem"style="height:798px"> 
+            <img src="<?php echo site_url('assets/images/home/carrossel/imagemCarrossel2.jpg') ?>" alt="Teste de Imagem"style="height:800px"> 
+            <img src="<?php echo site_url('assets/images/home/carrossel/imagemCarrossel3.jpg') ?>" alt="Teste de Imagem"style="height:680px"> 
+            <img src="<?php echo site_url('assets/images/home/carrossel/imagemCarrossel4.jpg') ?>" alt="Teste de Imagem"style="height:900px"> 
             <img src="<?php echo site_url('assets/images/home/carrossel/imagemCarrossel5.jpg') ?>" alt="Teste de Imagem"style="height:901px"> 
         </figure>
 
